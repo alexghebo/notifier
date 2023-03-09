ba060be17b0c878b8e5958fe2dffaddcf1a63e00
laurentiuvlaicu <laurentiu.vlaicu@verticaldigital.ca>
2023-03-09 13:06:53 +0200

    GenerationType changed in Sequence Lombok used Git rm .idea

--- src/main/java/ca/verticaldigital/notifier/entity/Person.java
+++ src/main/java/ca/verticaldigital/notifier/entity/Person.java
@@ -4,15 +4,20 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Date;
 
 @Entity
+@Getter @Setter @NoArgsConstructor @ToString
 public class Person {
 
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy= GenerationType.SEQUENCE)
     private long id;
     private String firstName;
     private String lastName;
@@ -21,9 +26,6 @@ public class Person {
     private String city;
     private boolean deleted;
 
-    public Person() {
-    }
-
     public Person(String firstName, String lastName, String email, Date birthdate, String city, boolean deleted) {
         this.firstName = firstName;
         this.lastName = lastName;
@@ -31,66 +33,5 @@ public class Person {
         this.birthdate = birthdate;
         this.city = city;
         this.deleted = deleted;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Date getBirthdate() {
-        return birthdate;
-    }
-
-    public void setBirthdate(Date birthdate) {
-        this.birthdate = birthdate;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public boolean isDeleted() {
-        return deleted;
-    }
-
-    public void setDeleted(boolean deleted) {
-        this.deleted = deleted;
-    }
-
-    @Override
-    public String toString() {
-        return "Person{" +
-                "id=" + id +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", email='" + email + '\'' +
-                ", birthdate=" + birthdate +
-                ", city='" + city + '\'' +
-                ", deleted=" + deleted +
-                '}';
     }
 }
